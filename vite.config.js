0b85503b0ee6208d8a913a4d532521492be4801d
Jhonny <carvalhoborgeshenrique@gmail.com>
2025-05-14 20:50:31 -0300

    Correção do deploy

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,11 @@
 import { defineConfig } from 'vite'
 import { svelte } from '@sveltejs/vite-plugin-svelte'
 
+// Substitua 'nome-do-repo' pelo NOME EXATO do seu repositório
 export default defineConfig({
+  base: '/final-project-mathemagraphia/',
   plugins: [svelte()],
-  base: '/final-project-mathemagraphia/'
+  server: {
+    hmr: false
+  }
 })
