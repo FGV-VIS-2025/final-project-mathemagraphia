25f031e27bfee6aaafd73bf8fcbaf35d404995dd
Jhonny <carvalhoborgeshenrique@gmail.com>
2025-05-14 20:34:32 -0300

    Teste2 Deploy

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
-// vite.config.js
-import { defineConfig } from 'vite';
-import svelte from '@sveltejs/vite-plugin-svelte';
+import { defineConfig } from 'vite'
+import { svelte } from '@sveltejs/vite-plugin-svelte'
 
 export default defineConfig({
   plugins: [svelte()],
-  base: '/final-project-matemagraphia/', // substitua pelo nome real
-});
+  base: '/final-project-mathemagraphia/'
+})
